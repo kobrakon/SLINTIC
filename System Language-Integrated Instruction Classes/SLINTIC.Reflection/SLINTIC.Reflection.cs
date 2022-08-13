8b003ca4bd430451ad1a5c4c2e584a01c968931b
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-08-13 00:15:13 -0400

    Update SLINTIC.Reflection.cs

--- System Language-Integrated Instruction Classes/SLINTIC.Reflection/SLINTIC.Reflection.cs
+++ System Language-Integrated Instruction Classes/SLINTIC.Reflection/SLINTIC.Reflection.cs
@@ -1,16 +1,23 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 // imagine adding protection levels for objects just to add reflection
 namespace SLINTIC.Reflection
 {
-    public class Reflection
+    public static class Reflection
     {
         ///<summary>
         ///Retrieves MethodBase from the specified method
         ///</summary>
-        public static MethodBase ?ReturnMethodFromClass<T>(string methodName) { return typeof(T).GetMethod(methodName); }
+        public static MethodBase ?ReturnMethodBase<T>(string methodName) => typeof(T).GetMethod(methodName);
         //Overload 1 for BindingFlags
-        public static MethodBase ?ReturnMethodFromClass<T>(string methodName, BindingFlags flags) { return typeof(T).GetMethod(methodName, flags); }
+        public static MethodBase ?ReturnMethodBase<T>(string methodName, BindingFlags flags) => typeof(T).GetMethod(methodName, flags);
+
+        ///<summary>
+        /// Retrieves MethodInfo from the specified method
+        ///</summary>
+        public static MethodInfo ?ReturnMethodInfo<T>(string methodName) => typeof(T).GetMethod(methodName);
+        //Overload 1 for BindingFlags
+        public static MethodInfo ?ReturnMethodInfo<T>(string methodName, BindingFlags flags) => typeof(T).GetMethod(methodName, flags);
 
         ///<summary>
         ///Accesses the given method and invokes it
@@ -28,5 +35,12 @@ namespace SLINTIC.Reflection
             if (method is null) return;
             method.Invoke(instance, parameters);
         }
+
+        ///<summary>
+        /// Gets requested field and replaces its value
+        ///</summary>
+        public static void Impose<T>(string fieldName, object ?instance,object ?fieldImport) => typeof(T).GetField(fieldName)?.SetValue(instance, fieldImport);
+        // Overload 1 for BindingFlags
+        public static void Impose<T>(string fieldName, BindingFlags flags, object? instance, object? fieldImport) => typeof(T).GetField(fieldName, flags)?.SetValue(instance, fieldImport);
     }
 }
